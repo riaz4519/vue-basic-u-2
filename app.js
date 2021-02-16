ae5e8625c78a26a5d39c5aac4f028b4ed396c293
riaz4519 <riaz.i3216@gmail.com>
2021-02-16 19:28:39 +0600

    14 dynamic class

--- app.js
+++ app.js
@@ -6,31 +6,18 @@ const  app = Vue.createApp({
             url:'https://www.google.com',
             showBooks:true,
             books:[
-                {title:'new book 1',author:'kew na 1',img:'assets/1.jpg'},
-                {title:'new book 2',author:'kew na 2',img:'assets/2.jpg'},
-                {title:'new book 3',author:'kew na 3',img:'assets/3.jpg'},
+                {title:'new book 1',author:'kew na 1',img:'assets/1.jpg',isFav:true},
+                {title:'new book 2',author:'kew na 2',img:'assets/2.jpg',isFav:false},
+                {title:'new book 3',author:'kew na 3',img:'assets/3.jpg',isFav:true},
             ]
 
         }
     },
     methods:{
         toggleShowBooks(){
-
             this.showBooks = !this.showBooks
-
         },
-        handleEvent(e,data) {
-            console.log(e,e.type)
-            if (data){
-                console.log(data)
-            }
-        },
-        handleMouseMove(e){
 
-            this.x = e.offsetX
-            this.y = e.offsetY
-
-        }
     }
 })
 
